9f95fd587944d8bc8cd2ce75cc7034ddfa1c4f96
Lou <lucaslolli@yahoo.com.br>
2024-09-25 20:59:32 -0300

    removed indentation from docstrings in data.py

--- data.py
+++ data.py
@@ -2,47 +2,45 @@ LOCATION_LIST = [
     [
         'Firewall Quarantine',
         """
-        The air is stifling and thick in this oppressive quarantine,
-        heavy with the heat radiating from the glowing runes etched into the stone walls.
-        Sweat beads on your skin as the temperature makes every breath feel like a struggle.
-        A suffocating silence is occasionally broken by the crackle of energy shields
-        and the low hum of distant overheating machinery.
+The air is stifling and thick in this oppressive quarantine,
+heavy with the heat radiating from the glowing runes etched into the stone walls.
+Sweat beads on your skin as the temperature makes every breath feel like a struggle.
+A suffocating silence is occasionally broken by the crackle of energy shields
+and the low hum of distant overheating machinery.
 
-        This is Firewall Quarantine, a crucible designed not just to contain threats,
-        but to break them, serving as a reminder of the harsh penalties for contamination in this realm.
+This is Firewall Quarantine, a crucible designed not just to contain threats,
+but to break them, serving as a reminder of the harsh penalties for contamination in this realm.
 
-        Inside the confines of a cell, you take in your surroundings.
-        The room is small, with iron bars separating you from a corridor beyond.
-        In the corner huddles another prisoner, his face partially obscured by shadows.
-        He shivers nervously, his eyes darting around the room, as if he expects something to happen...
+Inside the confines of a cell, you take in your surroundings.
+The room is small, with iron bars separating you from a corridor beyond.
+In the corner huddles another prisoner, his face partially obscured by shadows.
+He shivers nervously, his eyes darting around the room, as if he expects something to happen...
 
-        A massive door of reinforced metal locks the exit tightly.
-        It glows dimly with the light of its adorned runes.
+A massive door of reinforced metal locks the exit tightly.
+It glows dimly with the light of its adorned runes.
 
-        Through the bars, you can see the figure of a sentinel standing guard.
-        Its full-body metallic armor makes it impossible to tell if it's even human.
-        The sentinel's head scans the area with an unwavering gaze,
-        ensuring that escape is not an option.
+Through the bars, you can see the figure of a sentinel standing guard.
+Its full-body metallic armor makes it impossible to tell if it's even human.
+The sentinel's head scans the area with an unwavering gaze,
+ensuring that escape is not an option.
         """,
         [1],
         [
             (
                 'Prisoner',
                 [
-                    '''
-                You are... Aric Dataforge, right? I wasn't sure before, but now I can see...
-                They put me in a cell with the likes of you...?
+                    '''You are... Aric Dataforge, right? I wasn't sure before, but now I can see...
+They put me in a cell with the likes of you...?
 
-                This is all your fault! All that is happening to our kingdom, it's...
-                You are a Data Manipulator, after all! Protecting this realm was your sworn duty!
-                And yet... Look at what you brought upon us...
+This is all your fault! All that is happening to our kingdom, it's...
+You are a Data Manipulator, after all! Protecting this realm was your sworn duty!
+And yet... Look at what you brought upon us...
 
-                The way you can sense how Data flows through everything, the powerful Pycharms you can cast...
-                Your kind wielded forces most could barely comprehend, but now it's all gone...
-                Your abilities... They served no purpose but deceit...
+The way you can sense how Data flows through everything, the powerful Pycharms you can cast...
+Your kind wielded forces most could barely comprehend, but now it's all gone...
+Your abilities... They served no purpose but deceit...
 
-                Leave me be, Data Manipulator. I want nothing to do with you...
-                    ''',
+Leave me be, Data Manipulator. I want nothing to do with you...''',
                     'Hi!',
                     'Bye!'
                 ]
@@ -50,33 +48,27 @@ LOCATION_LIST = [
             (
                 'Sentinel',
                 [
-                    """
-                Despite it's stillness, there is a sense of something emanating from this armored figure.
-	            You can feel a faint, almost magnetic pull, as if something of importance lies within.
-	            Although unmistakeable, this fascination is as of right now only a mystery...
-                """
+                    """Despite it's stillness, there is a sense of something emanating from this armored figure.
+You can feel a faint, almost magnetic pull, as if something of importance lies within.
+Although unmistakeable, this fascination is as of right now only a mystery..."""
                 ]
             )
         ],
         [
             (
                 'Reinforced Metal Door',
-                '''
-        A massive, imposing door of reinforced metal.
+                '''A massive, imposing door of reinforced metal.
         It is adorned with runes that are somewhat familiar to you,
         but their meaning remains elusive.
         Three distinct locks are embedded into the frame, each uniquely designed.
-        It won't budge without the proper key, or keys...
-                ''')
+        It won't budge without the proper key, or keys...''')
         ]
     ],
     
     
     [
         'Terminal Square',
-        '''
-        It is square and terminal.
-        ''',
+        '''It is square and terminal.''',
         [], # connections
         [
             # npcs
